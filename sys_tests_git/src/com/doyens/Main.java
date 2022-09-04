ab449b49697babc3b06ca40eea78193743c56815
sys <1425546057@qq.com>
2022-09-04 17:00:20 +0800

    增加了show方法

--- sys_tests_git/src/com/doyens/Main.java
+++ sys_tests_git/src/com/doyens/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("你真是个傻逼");
+        int id = 10;
+        show(id);
+    }
+    public static void show(int id){
+        System.out.println(id);
     }
 }
